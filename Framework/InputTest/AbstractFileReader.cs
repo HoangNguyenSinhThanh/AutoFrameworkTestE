23c9018759db03670906f33c36b445d10b904bcd
M.S.I <M.S.I@SINHTHANH>
2022-04-21 00:06:45 +0700

    add input file cs

---
+++ Framework/InputTest/AbstractFileReader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelFileFramworkHandle
+{
+    public abstract class AbstractFileReader
+    {
+        public enum RuleTestStep
+        {
+
+        }
+        public abstract void OpenFile();
+        public abstract void CloseFile();
+        public abstract bool VerifySpec();
+        public abstract List<Testspec> GetData();
+        public void ParseData(List<Testspec> listTestpec)
+        {
+            List<string> testStepList = new List<string>();
+            foreach(var e in listTestpec)
+
+        }
+    }
+}
---
+++ Framework/InputTest/Testspec.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ExcelFileFramworkHandle
+{
+
+    public class Testspec
+    {
+        string description = "Description";
+        string testId = "TestId";
+        List<string> teststep = new List<string>();
+
+        public Testspec(List<string> listString)
+        {
+            var queue = new Queue<string>(listString);
+            description = queue.Dequeue();
+            testId = queue.Dequeue();
+            teststep = queue.ToList<string>();
+        }
+
+        public override string ToString()
+        {
+            return $"{description} - {testId} - {teststep}";
+        }
+    }
+}
